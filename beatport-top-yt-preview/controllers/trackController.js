c49f577660a5ced284fb92da4f9acf6bdba830b4
Nelson Li <greent911@gmail.com>
2019-09-18 11:34:51 +0800

    enhance controllers

--- beatport-top-yt-preview/controllers/trackController.js
+++ beatport-top-yt-preview/controllers/trackController.js
@@ -2,23 +2,26 @@ const { param, validationResult } = require('express-validator');
 const { trackService } = require('../services');
 const { InputFormatError } = require('../errors');
 
-function validateTypeInput() {
-  return param('type').matches(/^[a-zA-Z0-9]*$|^[a-zA-Z0-9]+[a-zA-Z0-9-]*[a-zA-Z0-9]+$/)
-    .withMessage('Incorrect input format');
-}
-
 exports.getTracks = [
-  validateTypeInput(),
-  async (req, res, next) => {
+  [
+    // VALIDATION RULES
+    // Check type from /:type, type must contain only letters and numbers (can be connected with dashes)
+    param('type').matches(/^[a-zA-Z0-9]*$|^[a-zA-Z0-9]+[a-zA-Z0-9-]*[a-zA-Z0-9]+$/)
+      .withMessage('Incorrect input format')
+  ],
+  (req, res, next) => {
+    // VALIDATE
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      let errArray = errors.array();
-      let message = errArray.reduce((prev, current) => {
-        return prev + current.msg + ' : ' + current.value + '\n';
-      }, '');
-      message = message.slice(0, -1);
-      return next(new InputFormatError(message));
+    if (errors.isEmpty()) {
+      return next();
     }
+    let errorMessage = errors.array().reduce((prev, current) => {
+      return prev + current.msg + ' : ' + current.value + '\n';
+    }, '').slice(0, -1);
+    return next(new InputFormatError(errorMessage));
+  },
+  async (req, res, next) => {
+    // HANDLE REQUEST
     let type = req.params.type || 'top100';
     try {
       let results = await trackService.getTracksByType(type);
