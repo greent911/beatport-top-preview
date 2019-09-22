f23bcd0adf57e0822e7df4c467e8dda98088fb75
Nelson Li <greent911@gmail.com>
2019-09-22 14:39:11 +0800

    fix SQL select

--- beatport-top-yt-preview/services/track/index.js
+++ beatport-top-yt-preview/services/track/index.js
@@ -4,6 +4,7 @@ const models = require('./../../models');
 const utils = require('./../../utils');
 
 /**
+ * The top track
  * @typedef {Object} Track
  * @property {number} num The track's top rank number
  * @property {string} type The track's type in database
@@ -16,6 +17,8 @@ const utils = require('./../../utils');
  * @property {string} link The track's page relative link on Beatport
  * @property {string} imglink The track's album artwork image link
  * @property {string} video_id The track's Youtube video ID
+ * @property {string} createdAt The number row data's created date
+ * @property {string} updatedAt The number row data's updated date
  */
 /**
  * @param {*} type
@@ -26,7 +29,7 @@ const getTracksByType = async (type) => {
   try {
     let tracks = await models.sequelize
       .query(
-        'SELECT num, type, title, artists, remixers, labels, genre, released, link, imglink, video_id ' +
+        'SELECT num, type, title, artists, remixers, labels, genre, released, link, imglink, video_id, createdAt, updatedAt ' +
           'FROM top_tracks ' + 
          'WHERE type = :type ' + 
            'AND num BETWEEN 1 AND 100 ' + 
